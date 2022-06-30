2228bd9ec6f2f56dd551058e2446580456d51946
neil <neil.danson@gmail.com>
2022-06-30 18:37:08 +0100

    Add timings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::time::Instant;
 use parser_combinator::language::lang_parser;
 use parser_combinator::language::*;
 use parser_combinator::vm::*;
@@ -40,20 +41,28 @@ fn main() {
         }
     }";
 
+    let parse_start = Instant::now();
     let expr = function.parse(program_source.trim_start());
-
+    let parse_end = Instant::now();
+    let parse_time = parse_end - parse_start;
     match expr {
         Result::Ok((result, remaining)) => {
+            let emit_start = Instant::now();
             let function = vm_emit::emit_function(&result);
+            let emit_end = Instant::now();
+            let emit_time = emit_end - emit_start;
 
-            println!("# AST   ##############################################");
+            println!("# AST   (Parsed {:?} ##############################################", parse_time);
             println!("{:#?} -> {:?}", result.body, remaining);
-            println!("# IL    ##############################################");
+            println!("# IL    (Emit   {:?} ##############################################", emit_time);
             print_il(&function.instructions);
             println!("# Result #############################################");
             let program = Program::new(HashMap::new());
+            let run_start = Instant::now();
             let result = program.eval(&function, &[]);
-            println!("{:?}", result);
+            let run_end = Instant::now();
+            let run_time = run_end - run_start;
+            println!("{:?} in {:?}", result, run_time);
         }, 
         Result::Err(error) => println!("{}", error)
     }
